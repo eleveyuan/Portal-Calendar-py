d65f8624e1993bb0544ca4e1ac6d2b0d1e3254cf
eleveyuan <eleveyuan@gmail.com>
2023-09-23 09:22:17 +0800

    update

--- src/main.py
+++ src/main.py
@@ -4,7 +4,27 @@
 """
     相当于原项目 portal-calender.ino 文件，也即入口文件
 """
+import network
+
 from config import *
 
 
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.scan()
 
+def startWifi():
+    if wlan.isconnected():
+        return True
+    
+    if WIFI_PASS != '':
+        wlan.connect(WIFI_NAME, WIFI_PASS)
+    else:
+        wlan.connect(WIFI_NAME, '')
+    
+    if wlan.isconnected():
+        return True
+
+
+def stopWifi():
+    pass
